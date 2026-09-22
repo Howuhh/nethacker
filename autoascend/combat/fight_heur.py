77d217eeb37609c1a44819811b40f3dc571ceea8
Howuhh <hsehowuhh@gmail.com>
2026-09-22 10:16:09 +0300

    nethackers evolve win

--- autoascend/combat/fight_heur.py
+++ autoascend/combat/fight_heur.py
@@ -15,7 +15,9 @@ from .utils import wielding_ranged_weapon, line_dis_from, inside
 
 def melee_monster_priority(agent, monsters, monster):
     _, y, x, mon, _ = monster
-    ret = 1
+    # hypothesis: finishing an adjacent ordinary monster sooner reduces the
+    # cumulative damage taken while the bot circles for a marginally better move.
+    ret = 3
     if agent.blstats.hitpoints > 8 or is_monster_faster(agent, monster):
         ret += 15
     if wielding_ranged_weapon(agent) and not is_monster_faster(agent, monster):
